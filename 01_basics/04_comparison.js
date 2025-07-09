bc7a5f4caa8af72001bb98fc3bc966516dcdce8c
Adan Akbar <adanakbarsiddiqui@gmail.com>
2025-07-09 21:48:54 +0500

    Update 04_comparison.js
    Added more depth to concepts.

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,11 +1,12 @@
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// Basic Conversion
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
 // Can't compare two different datatypes.
-console.log("2" > 1);
+console.log("2" > 1); // Perform type conversion. 2 > 1 so true.
 console.log("02" > 1);
 
 
@@ -17,6 +18,20 @@ console.log(null == undefined);
 
 // Undefined always gives false. Undefined is loosely equal to null.
 console.log(undefined == 0);
+console.log(undefined > 0); // undefine --> NaN
 console.log(undefined == null);
+
+// Strick Check
+console.log("2" == 2); // Performs conversion, so true.
+console.log("2" === 2); // No conersion. Compares values and also datatypes.
+
+// Avoid the following comparison. Clean code is key.
+
+console.log(undefined == 0);
+console.log(undefined == null);
+console.log(null > -1);
+console.log(null == 0); 
+console.log(null >= 0);
+console.log(null == undefined);
 
 
